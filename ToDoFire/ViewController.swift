ce3ecec549afd663dd0e87467ce416fcb04bfe9c
@Slava-Fly <school.94@bk.ru>
2023-10-01 13:56:57 +0800

    Initial Commit

---
+++ ToDoFire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoFire
+//
+//  Created by User on 01.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
